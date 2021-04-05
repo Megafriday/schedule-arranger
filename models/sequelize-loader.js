10054b02f2d4c16447034590bb4926709b4624af
megafriday <megafriday@yahoo.co.jp>
2021-04-06 02:14:51 +0900

    hero

--- models/sequelize-loader.js
+++ models/sequelize-loader.js
@@ -9,6 +9,8 @@ if (process.env.DATABASE_URL) {
 	option.dialectOptions.ssl = true;
 }
 
+console.log("★★★★★★process.env.DATABASE_URL：", process.env.DATABASE_URL);
+
 const sequelize = new Sequelize(
 	process.env.DATABASE_URL || 'postgres://postgres:pass@localhost/schedule_arranger',
 	option
